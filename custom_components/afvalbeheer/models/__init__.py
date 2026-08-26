08709062fa1a4b6c6c529fa9a4cbd268d8d844cb
Stijn <stijnvanderhoeven@outlook.com>
2026-08-26 19:24:30 +0200

    Update packages

--- custom_components/afvalbeheer/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Models for waste collection data.
-"""
-from .waste_collection import WasteCollection
-from .waste_repository import WasteCollectionRepository
-
-__all__ = ["WasteCollection", "WasteCollectionRepository"]
